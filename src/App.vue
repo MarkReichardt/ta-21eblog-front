aa01811282105b020ec4c84a616bf2ac75fdd8b0
Mark Reichardt <103030472+MarkReichardt@users.noreply.github.com>
2024-06-19 01:33:39 +0300

    base from teacher

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import MainNav from './components/MainNav.vue';
+import { useAuthStore } from './stores/auth.js';
+const auth = useAuthStore();
+auth.getUser();
+</script>
+<template>
+  <MainNav></MainNav>
+  <Suspense>
+    <RouterView></RouterView>
+  </Suspense>
+</template>
